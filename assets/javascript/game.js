8f85985b324c6de0674daa3c83972c05a9936e42
xiongkb <mkan-bx@hotmail.com>
2019-12-08 16:03:49 -0800

    added some starting variables and basic html

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+var words = ["chicken", "gravy", "tuna", "salmon", "kibbles", "fish", "beef", "turkey", "bread", "pumpkin", "mouse", "wands", 
+    "feather", "bells", "pompom", "tabby", "torbie", "tortoiseshell", "calico", "tree", "tunnel", "treats"];
+var winCount = 0;
+var loseCount =0;
