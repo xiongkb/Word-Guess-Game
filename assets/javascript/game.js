ebceca46562f9239817b00ec962f8b06024c21c6
xiongkb <mkan-bx@hotmail.com>
2019-12-10 17:36:30 -0800

    added for loops and function for user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,35 @@
-var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-var words = ["chicken", "gravy", "tuna", "salmon", "kibbles", "fish", "beef", "turkey", "bread", "pumpkin", "mouse", "wands", 
-    "feather", "bells", "pompom", "tabby", "torbie", "tortoiseshell", "calico", "tree", "tunnel", "treats"];
+// the arrays for the letters of the alphabet and words that will be picked randomly
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var words = ["chicken dinner", "gravy with food", "tuna", "salmon", "kibbles", "fish", "beef", "turkey", "pumpkin", "mouse wands", 
+    "feather toys", "bells and balls", "a tabby", "a torbie", "a tortoiseshell", "a calico", "cat tree", "tunnel for cat", "treats"];
+
+// variables for wins and loses, starting out at 0
 var winCount = 0;
-var loseCount =0;
+var loseCount = 0;
+
+// variables to/for grab the id from html
+
+var guessWord = document.getElementById("guess-Word");
+var chancesLeft = document.getElementById("chances-Left");
+var letterGuessed = document.getElementById("letter-Guessed");
+
+// variables for pc to pick a random and user letter input
+var randomWord = words[Math.floor(Math.random() * words.length)]
+var hiddenWord = "";
+
+
+
+// the function for words, should show _ _ _ _ for every character of the word
+for (var i = 0; i <randomWord.length; i++) {
+    hiddenWord += "_"
+    document.getElementById(".guessWord")
+}
+
+// User input by pressing on anything on the keyboard
+document.onkeyup = function(keypressed) {
+    var userInput = keypressed.key;
+
+    if (userInput === "a")
+    letterGuessed.append(userInput + " ");
+}
+
